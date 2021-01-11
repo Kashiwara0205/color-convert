d78ff2c4a5b971a9d6a1185785a04f72903d1c6d
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2021-01-11 12:47:28 +0900

    Add option validate

--- src/main.nim
+++ src/main.nim
@@ -3,19 +3,24 @@ import ../src/service/hexColorService
 import ../src/option/color_option
 import ../src/constants/constants
 
-
 proc shouldDispInstruction():bool = return paramCount() == 0 or paramCount() == 1
+
+proc convert(input: string, output: string): void = 
+  try:
+    let option = createColorOption(input, output)
+    case option.input
+    of I_HEX:
+      let service = hexColorService()
+      case option.output:
+      of O_HEX:
+        service.toHex(option.colorValue)
+      of O_RGB:
+        service.toRgb(option.colorValue)
+  except:
+    echo getCurrentExceptionMsg()
 
 if shouldDispInstruction():
   echo INSTRUCTIONS
   system.quit(QuitSuccess)
 
-let option = createColorOption(paramStr(1), paramStr(2))
-case option.input
-of I_HEX:
-  let service = hexColorService()
-  case option.output:
-  of O_HEX:
-    service.toHex(option.colorValue)
-  of O_RGB:
-    service.toRgb(option.colorValue)
+convert(paramStr(1), paramStr(2))
--- src/option/color_option.nim
+++ src/option/color_option.nim
@@ -1,10 +1,33 @@
+import sequtils
 import strutils
+import ../constants/constants
+
+proc validInputOptionForm(input: string): bool = 
+  if input.find(":") == -1: return false
+
+  let splitedinput = input.split(":")
+  if splitedinput.len != 2: return false
+
+  return true
+
+proc validInputOptionName(input: string): bool = 
+  let splitedinput = input.split(":")
+  return @[I_RGB, I_HEX].anyIt(it == splitedinput[0])
+
+proc validOutputOptionName(output: string): bool = 
+  return @[O_RGB, O_HEX].anyIt(it == output)
+
+proc validOption(input: string, output: string): void = 
+  if not validInputOptionForm(input): raise Exception.newException("Wrong input form")
+  if not validInputOptionName(input): raise Exception.newException("Not Exists Input Option")
+  if not validOutputOptionName(output): raise Exception.newException("Not Exists Output Option")
 
 type ColorOption* = object
   input*: string
   colorValue*: string
   output*: string
-  
-func createColorOption*(input: string, output: string): ColorOption = 
+
+proc createColorOption*(input: string, output: string): ColorOption = 
+  validOption(input, output)
   let splitedinput = input.split(":")
   return ColorOption(input: splitedinput[0], colorValue: splitedinput[1], output: output)
