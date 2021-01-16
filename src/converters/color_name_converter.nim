ff23095d50de61ce1ce00ad9d4cf505b3c66e2f7
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2021-01-16 16:01:04 +0900

    Change color definition

--- src/converters/color_name_converter.nim
+++ src/converters/color_name_converter.nim
@@ -12,18 +12,18 @@ func createColorNameConverter*(color: ColorName): ColorNameConverter =
 
 proc toRgb*(converterObj: ColorNameConverter): RgbColor = 
   case converterObj.color.getColorName:
-  of RED: return createRgb(230, 0, 18)
-  of GREEN: return createRgb(0, 153, 68)
-  of BLUE: return createRgb(29, 32, 136)
+  of RED: return createRgb(255, 0, 0)
+  of GREEN: return createRgb(0, 255, 0)
+  of BLUE: return createRgb(0, 0, 255)
   of YELLOW: return createRgb(255, 255, 0)
   of BLACK: return createRgb(0, 0, 0)
   of WHITE: return createRgb(255, 255, 255)
 
 proc toHex*(converterObj: ColorNameConverter): HexColor = 
   case converterObj.color.getColorName:
-  of RED: return createRgbColorConverter(createRgb(230, 0, 18)).toHex
-  of GREEN: return createRgbColorConverter(createRgb(0, 153, 68)).toHex
-  of BLUE: return createRgbColorConverter(createRgb(29, 32, 136)).toHex
+  of RED: return createRgbColorConverter(createRgb(255, 0, 0)).toHex
+  of GREEN: return createRgbColorConverter(createRgb(0, 255, 0)).toHex
+  of BLUE: return createRgbColorConverter(createRgb(0, 0, 255)).toHex
   of YELLOW: return createRgbColorConverter(createRgb(255, 255, 0)).toHex
   of BLACK: return createRgbColorConverter(createRgb(0, 0, 0)).toHex
   of WHITE: return createRgbColorConverter(createRgb(255, 255, 255)).toHex
--- test/converters/color_name_converter_test.nim
+++ test/converters/color_name_converter_test.nim
@@ -9,21 +9,21 @@ suite "description for ColorNameConverter":
   test "should convert to rgb":
     var colorNameConverter = createColorNameConverter(createColorName("Red"))
     var rgb = colorNameConverter.toRgb
-    check 230 == rgb.getRedColor
+    check 255 == rgb.getRedColor
     check 0 == rgb.getGreenColor
-    check 18 == rgb.getBlueColor
+    check 0 == rgb.getBlueColor
 
     colorNameConverter = createColorNameConverter(createColorName("Green"))
     rgb = colorNameConverter.toRgb
     check 0 == rgb.getRedColor
-    check 153 == rgb.getGreenColor
-    check 68 == rgb.getBlueColor
+    check 255 == rgb.getGreenColor
+    check 0 == rgb.getBlueColor
 
     colorNameConverter = createColorNameConverter(createColorName("Blue"))
     rgb = colorNameConverter.toRgb
-    check 29 == rgb.getRedColor
-    check 32 == rgb.getGreenColor
-    check 136 == rgb.getBlueColor
+    check 0 == rgb.getRedColor
+    check 0 == rgb.getGreenColor
+    check 255 == rgb.getBlueColor
 
     colorNameConverter = createColorNameConverter(createColorName("Yellow"))
     rgb = colorNameConverter.toRgb
@@ -45,13 +45,13 @@ suite "description for ColorNameConverter":
 
   test "should convert to hex":
     var colorNameConverter = createColorNameConverter(createColorName("Red"))
-    check "E60012" == colorNameConverter.toHex.getColor
+    check "FF0000" == colorNameConverter.toHex.getColor
 
     colorNameConverter = createColorNameConverter(createColorName("Green"))
-    check "009944" == colorNameConverter.toHex.getColor
+    check "00FF00" == colorNameConverter.toHex.getColor
   
     colorNameConverter = createColorNameConverter(createColorName("Blue"))
-    check "1D2088" == colorNameConverter.toHex.getColor
+    check "0000FF" == colorNameConverter.toHex.getColor
 
     colorNameConverter = createColorNameConverter(createColorName("Yellow"))
     check "FFFF00" == colorNameConverter.toHex.getColor
