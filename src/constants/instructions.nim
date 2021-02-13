f07a512d462eea6dcf8406f1dff36638d3e1fee5
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2021-02-13 15:09:19 +0900

    Update instructions

--- src/constants/instructions.nim
+++ src/constants/instructions.nim
@@ -9,13 +9,14 @@ color_convert --iHex:7B2D43 --oRgb
 color_convert --iRgb:123,45,67 --oHex
 => [ 7B2D43 ]
 
-
 ----------------------------------------------------
 Input option     | description
 ----------------------------------------------------
---iHex:{value}   | Input Hexadecimal. example: 7B2D43
 --iRgb:{value}   | Input RgbCode. example: 123,45,67
+--iHex:{value}   | Input Hexadecimal. example: 7B2D43
 --iColor:{value} | Input ColorName. example: Red
+--iCmyk:{value}  | Input Cmyk. example: 1,2,3,4
+--iHsl:{value}   | Input Hsl. example: 212, 43, 73
 ----------------------------------------------------
 
 ---------------------------------------------------
@@ -23,5 +24,7 @@ Out Option     | description
 ---------------------------------------------------
 --oRgb         | Output in Rgb 
 --oHex         | Output in Hexadecimal
+--oCmyk        | Output in Cmyk
+--oHsl         | Output in Hsl
 ---------------------------------------------------
 """
